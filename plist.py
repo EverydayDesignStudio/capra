bc5d9d26b07aa38d19c7403f4c6295fb68daf7c8
Tal Amram <tal89amram@gmail.com>
2018-09-27 10:38:47 -0700

    added plist

---
+++ plist.py
@@ -0,0 +1,49 @@
+#
+#    _________ _____  _________ _
+#   / ___/ __ `/ __ \/ ___/ __ `/
+#  / /__/ /_/ / /_/ / /  / /_/ /
+#  \___/\__,_/ .___/_/   \__,_/
+#           /_/
+# ===============================
+# Script for using Haishoku to detect dominant colour and palette from pictures
+# and writing them to a csv.
+# ===============================
+
+# Load modules
+import sys
+import csv
+sys.path.insert(0, "..")
+from haishoku.haishoku import Haishoku
+
+
+folder = "/Users/talamram/Downloads/thike/"
+
+
+# Count files in selected repository
+counter = 0
+for file in listdir('..'):
+    counter += 1
+
+# Iterate through images in folder and extract dominant colour and palette
+# and write them to a csv
+
+path = "/Users/talamram/Documents/haishoku/demo/demo_01.png"
+
+# getPalette api
+palette = Haishoku.getPalette(path)
+
+# getDominant api
+dominant = Haishoku.getDominant(path)
+
+
+
+
+
+
+
+# Haishoku object
+h = Haishoku.loadHaishoku(path)
+print('= showpalette =')
+print(h.palette)
+print('= dominant =')
+print(h.dominant)
