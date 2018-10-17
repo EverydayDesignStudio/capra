9a35d28c6445950bafce852e3c51b29de8aef6d7
Tal Amram <tal89amram@gmail.com>
2018-10-17 16:31:16 +0200

    1

---
+++ testcam_multiplexer.py
@@ -0,0 +1,34 @@
+from time import sleep
+import picamera
+import RPi.GPIO as gpio
+
+gpio.setmode(gpio.BCM)
+gpio.setup(22, gpio.OUT)
+gpio.setup(23, gpio.OUT)
+
+camera = picamera.PiCamera()
+camera.resolution = (1024, 768)
+#camera.start_preview()
+# Camera warm-up time
+sleep(2)
+
+
+
+
+
+camera.capture('foo.jpg')
+
+
+def selectcam(_cam):
+    if _cam < 1 or _cam > 3:
+        print '[selectcam] invalid cam number!'
+    else:
+        if _cam == 1:
+             gpio.output(22, False)
+             gpio.output(23, False)
+        if _cam == 2:
+             gpio.output(22, True)
+             gpio.output(23, False)
+        if _cam == 3:
+             gpio.output(22, True)
+             gpio.output(23, True)
