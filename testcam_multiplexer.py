368b782abf059a2f32d07cabe8adcb4e76a2b684
Tal Amram <tal_amram@sfu.ca>
2019-02-05 17:36:32 -0800

    1

--- testcam_multiplexer.py
+++ testcam_multiplexer.py
@@ -2,6 +2,23 @@ from time import sleep
 import picamera
 import RPi.GPIO as gpio
 
+def selectcam(_cam):
+    if _cam < 1 or _cam > 3:
+        print('[selectcam] invalid cam number!')
+    else:
+        if _cam == 1:
+             gpio.output(22, False)
+             gpio.output(23, False)
+        if _cam == 2:
+             gpio.output(22, True)
+             gpio.output(23, False)
+        if _cam == 3:
+             gpio.output(22, True)
+             gpio.output(23, True)
+
+
+
+
 gpio.setmode(gpio.BCM)
 gpio.setup(22, gpio.OUT) # FSA switch 1
 gpio.setup(23, gpio.OUT) # FSA switch 2
@@ -36,17 +53,3 @@ for i in range(15):
 # for cam in range(1, 4):
 #     selectcam(cam)
 #     camera.capture(str(cam) + '.jpg')
-
-def selectcam(_cam):
-    if _cam < 1 or _cam > 3:
-        print('[selectcam] invalid cam number!')
-    else:
-        if _cam == 1:
-             gpio.output(22, False)
-             gpio.output(23, False)
-        if _cam == 2:
-             gpio.output(22, True)
-             gpio.output(23, False)
-        if _cam == 3:
-             gpio.output(22, True)
-             gpio.output(23, True)
