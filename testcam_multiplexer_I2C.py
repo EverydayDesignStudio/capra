e135fc4e5f9eb34052ebb9effe565a5b33097a8e
Tal Amram <tal_amram@sfu.ca>
2019-02-14 13:18:55 -0800

    1

--- testcam_multiplexer_I2C.py
+++ testcam_multiplexer_I2C.py
@@ -4,28 +4,28 @@
 # Change channel of TCA9548A
 # Example: sudo ./multiplexer_channel.py 0
 
-# import smbus
-# import time
-# import sys
-#
-# I2C_address = 0x77
-# I2C_bus_number = 1
-# I2C_ch_0 = 0b00000001
-# I2C_ch_1 = 0b00000010
-# I2C_ch_2 = 0b00000100
-# I2C_ch_3 = 0b00001000
-# I2C_ch_4 = 0b00010000
-# I2C_ch_5 = 0b00100000
-# I2C_ch_6 = 0b01000000
-# I2C_ch_7 = 0b10000000
-#
-# def I2C_setup(i2c_channel_setup):
-#     bus = smbus.SMBus(I2C_bus_number)
-#     bus.write_byte(I2C_address,i2c_channel_setup)
-#     time.sleep(0.1)
-#     print "TCA9548A I2C channel status:", bin(bus.read_byte(I2C_address))
-#
-# I2C_setup(int(sys.argv[1]))
+import smbus
+import time
+import sys
+
+I2C_address = 0x77
+I2C_bus_number = 1
+I2C_ch_0 = 0b00000001
+I2C_ch_1 = 0b00000010
+I2C_ch_2 = 0b00000100
+I2C_ch_3 = 0b00001000
+I2C_ch_4 = 0b00010000
+I2C_ch_5 = 0b00100000
+I2C_ch_6 = 0b01000000
+I2C_ch_7 = 0b10000000
+
+def I2C_setup(i2c_channel_setup):
+    bus = smbus.SMBus(I2C_bus_number)
+    bus.write_byte(I2C_address,i2c_channel_setup)
+    time.sleep(0.1)
+    print "TCA9548A I2C channel status:", bin(bus.read_byte(I2C_address))
+
+I2C_setup(int(sys.argv[1]))
 
 
 
