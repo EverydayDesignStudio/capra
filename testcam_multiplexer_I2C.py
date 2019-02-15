1c15d2508f6634cc3d93c009e8d1decd157044ed
Tal Amram <tal_amram@sfu.ca>
2019-02-15 12:17:16 -0800

    rst

--- testcam_multiplexer_I2C.py
+++ testcam_multiplexer_I2C.py
@@ -10,6 +10,7 @@ import sys
 
 I2C_address = 0x70
 I2C_bus_number = 1
+I2C_RST = 0b00000000
 I2C_ch_0 = 0b00000001
 I2C_ch_1 = 0b00000010
 I2C_ch_2 = 0b00000100
@@ -25,7 +26,7 @@ def I2C_setup(i2c_channel_setup):
     time.sleep(0.1)
     print "TCA9548A I2C channel status:", bin(bus.read_byte(I2C_address))
 
-I2C_setup(I2C_address)
+I2C_setup(I2C_RST)
 I2C_setup(I2C_ch_1)
 
 
