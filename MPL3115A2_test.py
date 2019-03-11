5642bd6adff9bf60c38e7e638c3790486b1bac11
Tal Amram <hallo@talamram.nl>
2019-03-11 16:55:47 -0700

    1

--- MPL3115A2_test.py
+++ MPL3115A2_test.py
@@ -10,51 +10,53 @@ import time
 # Get I2C bus
 bus = smbus.SMBus(1)
 
-# MPL3115A2 address, 0x60(96)
-# Select control register, 0x26(38)
-#		0xB9(185)	Active mode, OSR = 128, Altimeter mode
-bus.write_byte_data(0x60, 0x26, 0xB9)
-# MPL3115A2 address, 0x60(96)
-# Select data configuration register, 0x13(19)
-#		0x07(07)	Data ready event enabled for altitude, pressure, temperature
-bus.write_byte_data(0x60, 0x13, 0x07)
-# MPL3115A2 address, 0x60(96)
-# Select control register, 0x26(38)
-#		0xB9(185)	Active mode, OSR = 128, Altimeter mode
-bus.write_byte_data(0x60, 0x26, 0xB9)
 
-time.sleep(1)
+while(1):
+    # MPL3115A2 address, 0x60(96)
+    # Select control register, 0x26(38)
+    #		0xB9(185)	Active mode, OSR = 128, Altimeter mode
+    bus.write_byte_data(0x60, 0x26, 0xB9)
+    # MPL3115A2 address, 0x60(96)
+    # Select data configuration register, 0x13(19)
+    #		0x07(07)	Data ready event enabled for altitude, pressure, temperature
+    bus.write_byte_data(0x60, 0x13, 0x07)
+    # MPL3115A2 address, 0x60(96)
+    # Select control register, 0x26(38)
+    #		0xB9(185)	Active mode, OSR = 128, Altimeter mode
+    bus.write_byte_data(0x60, 0x26, 0xB9)
 
-# MPL3115A2 address, 0x60(96)
-# Read data back from 0x00(00), 6 bytes
-# status, tHeight MSB1, tHeight MSB, tHeight LSB, temp MSB, temp LSB
-data = bus.read_i2c_block_data(0x60, 0x00, 6)
+    time.sleep(1)
 
-# Convert the data to 20-bits
-tHeight = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
-# temp = ((data[4] * 256) + (data[5] & 0xF0)) / 16
-altitude = tHeight / 16.0
-# cTemp = temp / 16.0
-# fTemp = cTemp * 1.8 + 32
+    # MPL3115A2 address, 0x60(96)
+    # Read data back from 0x00(00), 6 bytes
+    # status, tHeight MSB1, tHeight MSB, tHeight LSB, temp MSB, temp LSB
+    data = bus.read_i2c_block_data(0x60, 0x00, 6)
 
-# MPL3115A2 address, 0x60(96)
-# Select control register, 0x26(38)
-#		0x39(57)	Active mode, OSR = 128, Barometer mode
-bus.write_byte_data(0x60, 0x26, 0x39)
+    # Convert the data to 20-bits
+    tHeight = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
+    # temp = ((data[4] * 256) + (data[5] & 0xF0)) / 16
+    altitude = tHeight / 16.0
+    # cTemp = temp / 16.0
+    # fTemp = cTemp * 1.8 + 32
 
-time.sleep(1)
+    # MPL3115A2 address, 0x60(96)
+    # Select control register, 0x26(38)
+    #		0x39(57)	Active mode, OSR = 128, Barometer mode
+    bus.write_byte_data(0x60, 0x26, 0x39)
 
-# MPL3115A2 address, 0x60(96)
-# Read data back from 0x00(00), 4 bytes
-# status, pres MSB1, pres MSB, pres LSB
-# data = bus.read_i2c_block_data(0x60, 0x00, 4)
+    time.sleep(1)
 
-# Convert the data to 20-bits
-# pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
-# pressure = (pres / 4.0) / 1000.0
+    # MPL3115A2 address, 0x60(96)
+    # Read data back from 0x00(00), 4 bytes
+    # status, pres MSB1, pres MSB, pres LSB
+    # data = bus.read_i2c_block_data(0x60, 0x00, 4)
 
-# Output data to screen
-# print "Pressure : %.2f kPa" %pressure
-print "Altitude : %.2f m" %altitude
-# print "Temperature in Celsius  : %.2f C" %cTemp
-# print "Temperature in Fahrenheit  : %.2f F" %fTemp
+    # Convert the data to 20-bits
+    # pres = ((data[1] * 65536) + (data[2] * 256) + (data[3] & 0xF0)) / 16
+    # pressure = (pres / 4.0) / 1000.0
+
+    # Output data to screen
+    # print "Pressure : %.2f kPa" %pressure
+    print "Altitude : %.2f m" %altitude
+    # print "Temperature in Celsius  : %.2f C" %cTemp
+    # print "Temperature in Fahrenheit  : %.2f F" %fTemp
