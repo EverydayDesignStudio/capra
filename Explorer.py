05504ad770e9165ffe253cbd6b77838958e84116
Tal Amram <hallo@talamram.nl>
2019-04-24 14:39:54 -0700

    Remove empty lines

--- Explorer.py
+++ Explorer.py
@@ -27,8 +27,6 @@ LED_GREEN = 24
 LED_BTM = 26
 LED_AMBER = 27
 
-
-
 # Get I2C bus
 bus = smbus.SMBus(1)
 
@@ -138,15 +136,12 @@ def selectcam(_cam):
         if _cam == 1:
              gpio.output(SEL_1, False)
              gpio.output(SEL_2, False)
-
         if _cam == 2:
              gpio.output(SEL_1, True)
              gpio.output(SEL_2, False)
-
         if _cam == 3:
              gpio.output(SEL_1, True)
              gpio.output(SEL_2, True)
-
         time.sleep(0.1)
 
 def writedata(index, timestamp, altitude):
