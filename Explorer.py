3ea488f3bff5b6fada5cde3d45fe1d6c0fb1f78b
Tal Amram <hallo@talamram.nl>
2019-04-25 12:19:21 -0700

    Remove camera3

--- Explorer.py
+++ Explorer.py
@@ -181,7 +181,6 @@ while(1):
     # -------------------------------------
     camcapture(1)
     camcapture(2)
-    camcapture(3)
 
     # MPL3115A2 address, 0x60(96)
     # Read data back from 0x00(00), 6 bytes
