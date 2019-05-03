fb8767d9fac4974b226961b43d247ba964b944e3
Tal Amram <hallo@talamram.nl>
2019-05-03 13:49:04 -0700

    Add printlines to camselect

--- Explorer.py
+++ Explorer.py
@@ -108,14 +108,17 @@ def camcapture(_camno):
         print('[selectcam] invalid cam number!')
     else:
         if _camno == 1:
-             gpio.output(SEL_1, False)
-             gpio.output(SEL_2, False)
+            print("select cam 1")
+            gpio.output(SEL_1, False)
+            gpio.output(SEL_2, False)
         if _camno == 2:
-             gpio.output(SEL_1, True)
-             gpio.output(SEL_2, False)
+            print("select cam 2")
+            gpio.output(SEL_1, True)
+            gpio.output(SEL_2, False)
         if _camno == 3:
-             gpio.output(SEL_1, True)
-             gpio.output(SEL_2, True)
+            print("select cam 3")
+            gpio.output(SEL_1, True)
+            gpio.output(SEL_2, True)
         time.sleep(0.2)
         photoname = dir + folder + str(photono) + '_cam' + str(_camno) + '.jpg'
         print(photoname)
--- lastRow.py
+++ lastRow.py
@@ -1,9 +1,11 @@
-
+import csv
 
 csvfile = '/Users/talamram/Documents/CapraHikes/hike21/meta.csv'
-  with open(csvfile, 'r') as meta:
-      reader = csv.reader(meta)
-      lasthikephoto = 0
-      lasthikedate = 0
-      row_count = sum(1 for row in reader)
-      print("row count:", str(row_count))
+with open(csvfile, 'r') as meta:
+    reader = csv.reader(meta)
+    lasthikephoto = 0
+    lasthikedate = 0
+    row_count = sum(1 for row in reader)
+    print("row count:", str(row_count))
+    reader.seek(row_count-1)
+    print(meta.getRow(0))
