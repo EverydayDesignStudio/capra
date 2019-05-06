d2081c4d3184899ca9bb87d265b15a437289a465
Tal Amram <hallo@talamram.nl>
2019-05-06 15:36:26 -0700

    Fix syntax

--- Explorer.py
+++ Explorer.py
@@ -137,7 +137,7 @@ blink(LED_AMBER, 2, 0.1) #computer says hello
 blink(LED_BTM, 2, 0.1) #computer says hello
 
 # Initialize camera object
-print(initializing camera)
+print('initializing camera')
 gpio.output(SEL_1, False)
 gpio.output(SEL_2, False)
 time.sleep(0.1)
