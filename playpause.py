e3c7b553e8b60a8146226b74c075890df3811fad
Tal Amram <hallo@talamram.nl>
2019-06-26 17:10:23 -0700

    Add boolean inversion

--- playpause.py
+++ playpause.py
@@ -8,11 +8,12 @@ BUTTON_PLAYPAUSE = 17 # BOARD - 11
 gpio.setmode(gpio.BCM)
 gpio.setup(BUTTON_PLAYPAUSE, gpio.IN)
 
-try:
-    print("waiting for edge")
-    gpio.wait_for_edge(BUTTON_PLAYPAUSE, gpio.RISING)
-    shared.pause != shared.pause
-    print("pause = ", shared.pause)
+while(True):
+    try:
+        print("waiting for edge")
+        gpio.wait_for_edge(BUTTON_PLAYPAUSE, gpio.RISING)
+        shared.pause = not shared.pause
+        print("pause = ", shared.pause)
 
-except KeyboardInterrupt:
-    print("Interrupted")
+    except KeyboardInterrupt:
+        print("Interrupted")
