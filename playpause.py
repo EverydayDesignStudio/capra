227c50234bbdf5c7ef1152fc02a83336b3fdcb46
Tal Amram <hallo@talamram.nl>
2019-06-27 16:10:30 -0700

    Add threading

--- playpause.py
+++ playpause.py
@@ -8,14 +8,43 @@ BUTTON_PLAYPAUSE = 17 # BOARD - 11
 gpio.setmode(gpio.BCM)
 gpio.setup(BUTTON_PLAYPAUSE, gpio.IN)
 
-def interrupt():
-    while(True):
-        try:
-            print("waiting for edge")
-            gpio.wait_for_edge(BUTTON_PLAYPAUSE, gpio.RISING)
-            shared.pause = not shared.pause
-            print("pause = ", shared.pause)
-            time.wait(0.5)
+class Playpause:
+    def __init__(self):
+        self._running = True
 
-        except KeyboardInterrupt:
-            print("Interrupted")
+    def terminate(self):
+        self._running = False
+
+    def run(self):
+        global pause
+        while self._running:
+            try:
+                print("waiting")
+                gpio.wait_for_edge(BUTTON_PLAYPAUSE, gpio.RISING)
+                status = not status
+                print("=====================")
+                time.sleep(2)
+            except:
+                pass
+
+#Create Class
+PP_INTERRUPT = Playpause()
+#Create Thread
+PP_THREAD = Thread(target=PP_INTERRUPT.run)
+#Start Thread
+PP_THREAD.start()
+
+
+
+
+# def interrupt():
+#     while(True):
+#         try:
+#             print("waiting for edge")
+#             gpio.wait_for_edge(BUTTON_PLAYPAUSE, gpio.RISING)
+#             shared.pause = not shared.pause
+#             print("pause = ", shared.pause)
+#             time.wait(0.5)
+#
+#         except KeyboardInterrupt:
+#             print("Interrupted")
