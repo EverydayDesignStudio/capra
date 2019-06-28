a3155d035cd62ade78e292f3f262d7194b03c86a
Tal Amram <hallo@talamram.nl>
2019-06-27 17:30:40 -0700

    Add turn off code

---
+++ utils/turn-off.py
@@ -0,0 +1,17 @@
+#  Turning off the Raspberry pi
+# =================================================
+#
+import time
+import shared
+import RPi.GPIO as gpio
+
+
+BUTTON_OFF = 3 #25 # BOARD - 22
+
+
+gpio.setmode(gpio.BCM)
+gpio.setup(BUTTON_OFF, gpio.IN)
+
+while(True):
+    gpio.wait_for_edge(BUTTON_OFF, gpio.RISING)
+    print("Turning off pi")
