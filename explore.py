973762a3ab499be1d4810a3a00930a371ec57c92
Jordan White <jordanrw@vt.edu>
2019-06-28 17:59:46 -0700

    Fix camera choice logic, remove non-pi db logic, add initial led action to funciton

--- explore.py
+++ explore.py
@@ -14,16 +14,10 @@ import smbus  # For interfacing over I2C with the altimeter
 import picamera  # For interfacting with the PiCamera
 import RPi.GPIO as gpio
 
-is_RPi = True
-if is_RPi:
-    DB = '/home/pi/capra-storage/capra_explorer.db'
-    DIRECTORY = '/home/pi/capra-storage/'
-else:
-    DB = '/Volumes/Capra/capra-storage/capra_explorer.db'
-    DIRECTORY = '/Volumes/Capra/capra-storage/'
+DB = '/home/pi/capra-storage/capra_camera.db'
+DIRECTORY = '/home/pi/capra-storage/'
 
-# Pin configuration
-# Will have more added later on to accomodate on/off switch
+# Pin configuration -- will add more later to accomodate on/off switch
 BUTTON_PLAYPAUSE = 4
 SEL_1 = 22
 SEL_2 = 23
@@ -42,13 +36,6 @@ gpio.setup(LED_GREEN, gpio.OUT)  # status led1
 gpio.setup(LED_AMBER, gpio.OUT)  # status led2
 gpio.setup(LED_BTM, gpio.OUT)  # status led3
 
-# Turn off LEDs
-gpio.output(LED_GREEN, True)
-time.sleep(0.1)
-gpio.output(LED_AMBER, True)
-time.sleep(0.1)
-gpio.output(LED_BTM, False)
-
 # Set Variables
 RESOLUTION = (1280, 720)
 # RESOLUTION = (720, 405)
@@ -56,6 +43,15 @@ RESOLUTION = (1280, 720)
 # NEW_HIKE_TIME = 43200  # 12 hours
 NEW_HIKE_TIME = 21600  # 6 hours
 # NEW_HIKE_TIME = 10800  # 3 hours
+
+
+# Turning off LEDs
+def turn_off_leds():
+    gpio.output(LED_GREEN, True)
+    time.sleep(0.1)
+    gpio.output(LED_AMBER, True)
+    time.sleep(0.1)
+    gpio.output(LED_BTM, False)
 
 
 # For blinking LEDs
@@ -119,11 +115,11 @@ def camcapture(pi_cam: picamera, cam_num: int, hike_num: int, photo_index: int, 
         raise Exception('{n} is an invalid camera number. It must be 1, 2, or 3.'.format(n=cam_num))
     else:
         if cam_num == 1:
-            gpio.output(SEL_1, False)
+            gpio.output(SEL_1, True)
             gpio.output(SEL_2, False)
             print("cam 1 selected")
         if cam_num == 2:
-            gpio.output(SEL_1, True)
+            gpio.output(SEL_1, False)
             gpio.output(SEL_2, False)
             print("cam 2 selected")
         if cam_num == 3:
@@ -143,6 +139,7 @@ def camcapture(pi_cam: picamera, cam_num: int, hike_num: int, photo_index: int, 
 
 
 def main():
+    turn_off_leds()
     hello_blinks()
     pi_cam = initialize_picamera()
     create_or_continue_hike()
