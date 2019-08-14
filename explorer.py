9b4068ac443930e2230e175c4cbcfca7f3b15780
jordanrw <jordan_white@sfu.ca>
2019-08-14 14:00:14 -0700

    Remove all keyboard controls and ifRPi conditions

--- explorer.py
+++ explorer.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 
-is_RPi = True
-
-if is_RPi:
-    DB = '/home/pi/Pictures/capra-projector.db'
-    PATH = '/home/pi/Pictures'
-else:
-    DB = '/Volumes/Capra/capra-projector.db'
-    PATH = '/Volumes/Capra'
-blank_path = '{p}/blank.png'.format(p=PATH)
+# ------------------------------------------------------------------------------
+#  Slideshow program for the Explorer projector unit. Displays pictures and
+#  allows navigation through them based on 3 modes: time, altitude, and color
+# ------------------------------------------------------------------------------
 
 # Imports
 from classes.capra_data_types import Picture, Hike
 from classes.sql_controller import SQLController
 from classes.sql_statements import SQLStatements
+
 from PIL import ImageTk, Image          # Pillow image functions
 from tkinter import Tk, Canvas, Label   # Tkinter, GUI framework in use
 import time
 from time import sleep
 import datetime
-if is_RPi:
-    from gpiozero import Button         # Rotary encoder, detected as button
-    from RPi import GPIO                # GPIO pin detection for Raspberry Pi
+
+from gpiozero import Button         # Rotary encoder, detected as button
+from RPi import GPIO                # GPIO pin detection for Raspberry Pi
+
+# Database location
+DB = '/home/pi/Pictures/capra-projector.db'
+PATH = '/home/pi/Pictures'
+blank_path = '{p}/blank.png'.format(p=PATH)
 
 # GPIO BCM PINS
 ROTARY_ENCODER_CLOCKWISE = 23
@@ -45,36 +46,35 @@ SLIDER_SWITCH_MODE_3 = 0
 
 
 # Setup GPIO
-if is_RPi:
-    GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM)
 
-    # Rotary encoder
-    clk = ROTARY_ENCODER_CLOCKWISE
-    cnt = ROTARY_ENCODER_COUNTER
-    rotary_button = ROTARY_ENCODER_BUTTON
-    GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(cnt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(rotary_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Rotary encoder
+clk = ROTARY_ENCODER_CLOCKWISE
+cnt = ROTARY_ENCODER_COUNTER
+rotary_button = ROTARY_ENCODER_BUTTON
+GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(cnt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(rotary_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    # Rotary switch
-    mode1 = 13
-    mode2 = 19
-    mode3 = 26
-    GPIO.setup(mode1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(mode2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(mode3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Rotary switch
+mode1 = 13
+mode2 = 19
+mode3 = 26
+GPIO.setup(mode1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(mode2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(mode3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    # Play / Pause button
-    play_pause_button = 5
-    GPIO.setup(play_pause_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Play / Pause button
+play_pause_button = 5
+GPIO.setup(play_pause_button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 
 # Slideshow class which is the main class that runs and is listening for events
 class Slideshow:
     # Setup for the batch of images
-    TRANSITION_DELAY = 2000  # time between pictures in milliseconds
-    IS_TRANSITION_FORWARD = True
-    ROTARY_COUNT = 0  # Used exclusively for testing
+    TRANSITION_DELAY = 2000         # Time between pictures in milliseconds
+    IS_TRANSITION_FORWARD = True    # Update  auto-advance direction
+    ROTARY_COUNT = 0                # Used exclusively for testing
 
     def __init__(self, win):
         # Setup the window
@@ -87,20 +87,15 @@ class Slideshow:
         # self.canvas.configure(bg='#444')
         self.canvas.pack(expand='yes', fill='both')
 
-        # Bind to events in which to listen
-        self.window.bind('<Left>', self.leftKey)
-        self.window.bind('<Right>', self.rightKey)
-        self.window.bind('<space>', self.space_key)
+        # Bind to hardware control events
+        self.window.bind(GPIO.add_event_detect(clk, GPIO.BOTH, callback=self.detected_rotary_change))
+        self.clkLastState = GPIO.input(clk)
 
-        if is_RPi:
-            self.window.bind(GPIO.add_event_detect(clk, GPIO.BOTH, callback=self.detected_rotary_change))
-            self.clkLastState = GPIO.input(clk)
-
-            # Using GPIO.BOTH and GPIO input state, however I have noticed glitches. This will need to be ironed out
-            # May need to use an after loop
-            self.window.bind(GPIO.add_event_detect(rotary_button, GPIO.BOTH, callback=self.rotary_button_pressed))
-            self.rotary_button_state = GPIO.input(rotary_button)
-            self.rotary_button_state = not self.rotary_button_state  # default is True, so set it to False
+        # Using GPIO.BOTH and GPIO input state, however I have noticed glitches. This will need to be ironed out
+        # May need to use an after loop
+        self.window.bind(GPIO.add_event_detect(rotary_button, GPIO.BOTH, callback=self.rotary_button_pressed))
+        self.rotary_button_state = GPIO.input(rotary_button)
+        self.rotary_button_state = not self.rotary_button_state  # default is True, so set it to False
 
         # Initialization for database implementation
         self.sql_controller = SQLController(database=DB)
@@ -223,30 +218,6 @@ class Slideshow:
 
         root.after(self.TRANSITION_DELAY, self.auto_increment_slideshow)
 
-    # KEYBOARD KEYS
-    def rightKey(self, event):
-        # print('increment the count')
-        self.IS_TRANSITION_FORWARD = True
-        # self.picture = self.sql_controller.next_altitude_picture_across_hikes(self.picture)
-        self.picture = self.sql_controller.next_time_picture_in_hike(self.picture)
-        # self.picture.print_obj()
-        self._build_next_raw_images(self.picture)
-        self.alpha = .2     # Resets amount of fade between pictures
-        self.update_text()
-
-    def leftKey(self, event):
-        # print("decrement the count")
-        self.IS_TRANSITION_FORWARD = False
-        # self.picture = self.sql_controller.previous_altitude_picture_across_hikes(self.picture)
-        self.picture = self.sql_controller.previous_time_picture_in_hike(self.picture)
-        # self.picture.print_obj()
-        self._build_next_raw_images(self.picture)
-        self.alpha = .2     # Resets amount of fade between pictures
-        self.update_text()
-
-    def space_key(self, event):
-        print('space key pressed')
-
     # HARDWARE CONTROLS
     def detected_rotary_change(self, event):
         clkState = GPIO.input(clk)
@@ -256,14 +227,14 @@ class Slideshow:
         if clkState != self.clkLastState:
             # Increment
             if cntState != clkState:
+                self.IS_TRANSITION_FORWARD = True  # For auto slideshow
+
                 if (self.rotary_button_state):
                     print('INCREMENT ACROSS ALL HIKES')
                     self.picture = self.sql_controller.next_time_picture_across_hikes(self.picture)
                 else:
                     self.ROTARY_COUNT += 1
                     print("Rotary +: ", self.ROTARY_COUNT)
-
-                    self.IS_TRANSITION_FORWARD = True   # For auto slideshow
                     self.picture = self.sql_controller.next_time_picture_in_hike(self.picture)
                     # self.picture.print_obj()
                     self._build_next_raw_images(self.picture)
@@ -272,14 +243,14 @@ class Slideshow:
                     # self.update_tick()
             # Decrement
             else:
+                self.IS_TRANSITION_FORWARD = False  # For auto slideshow
+
                 if (self.rotary_button_state):
                     print('DECREMENT ACROSS ALL HIKES')
                     self.picture = self.sql_controller.next_time_picture_across_hikes(self.picture)
                 else:
                     self.ROTARY_COUNT -= 1
                     print("Rotary -: ", self.ROTARY_COUNT)
-
-                    self.IS_TRANSITION_FORWARD = False  # For auto slideshow
                     self.picture = self.sql_controller.previous_time_picture_in_hike(self.picture)
                     # self.picture.print_obj()
                     self._build_next_raw_images(self.picture)
