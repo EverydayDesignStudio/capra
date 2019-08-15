8dbc0382fe721d16ebfbaf9c0a27179fac3163ff
Tal Amram <tal89amram@gmail.com>
2019-08-15 15:41:15 -0700

    Error logger

--- collector.py
+++ collector.py
@@ -34,44 +34,21 @@ SEL_2 = 23              # BOARD - 16
 LED_GREEN = 24          # BOARD - 18
 LED_BTM = 26            # BOARD - 37
 LED_AMBER = 27          # BOARD - 13
-PIEZO = 18              # BOARD - 12
-
-# TODO enable these values when switching to manufactured Buttonboard
-# LED_GREEN = 26         # BOARD - 37
-# LED_AMBER = 12         # BOARD - 33
-
 
 # Set file wide shared variables
 RESOLUTION = (1280, 720)
 # RESOLUTION = (720, 405)
-# NEW_HIKE_TIME = 43200     # 12 hours
-# NEW_HIKE_TIME = 21600     # 6 hours
-# NEW_HIKE_TIME = 10800     # 3 hours
-# NEW_HIKE_TIME = 5400        # 1 1/2 hours
-# NEW_HIKE_TIME = 1800      # 1/2 hour
-NEW_HIKE_TIME = 1
+# NEW_HIKE_TIME = 43200  # 12 hours
+NEW_HIKE_TIME = 21600  # 6 hours
+# NEW_HIKE_TIME = 10800  # 3 hours
 
-# Piezo frequencies
-c = 261
-d = 294
-e = 329
-f = 349
-g = 392
-a = 440
-b = 493
-C = 423
-
-
-# Initialize GPIO pins
-def initialize_GPIOs():
-    gpio.setwarnings(False)
-    gpio.setmode(gpio.BCM)
-    gpio.setup(SEL_1, gpio.OUT)         # select 1
-    gpio.setup(SEL_2, gpio.OUT)         # select 2
-    gpio.setup(LED_GREEN, gpio.OUT)     # status led1
-    gpio.setup(LED_AMBER, gpio.OUT)     # status led2
-    gpio.setup(LED_BTM, gpio.OUT)       # status led3
-    gpio.setup(PIEZO, gpio.OUT)         # PIEZO
+gpio.setwarnings(False)
+gpio.setmode(gpio.BCM)
+gpio.setup(SEL_1, gpio.OUT)         # select 1
+gpio.setup(SEL_2, gpio.OUT)         # select 2
+gpio.setup(LED_GREEN, gpio.OUT)     # status led1
+gpio.setup(LED_AMBER, gpio.OUT)     # status led2
+gpio.setup(LED_BTM, gpio.OUT)       # status led3
 
 
 # Turn off LEDs
@@ -104,38 +81,17 @@ def hello_blinks():
     blink(LED_BTM, 2, 0.1)
 
 
-def blink_after_crash():
-    blink_time = 0
-    while (blink_time < 10):
-        blink(LED_BTM, 4, 0.1)
-        time.sleep(0.5)
-        blink_time += 1
-
-
-# Sound a beep from the piezo element
-def beep(tone, duration, repeat):
-    pzo = gpio.pwm(PIEZO, 0) # initialize piezo object
-    for r in range(repeat):
-        pzo.start(100)
-        pzo.ChangeFrequency(tone)
-        time.sleep(duration)
-        pzo.stop()
-        time.sleep(0.1)
-
-
 # Initialize and return picamera object
 def initialize_picamera(resolution: tuple) -> picamera:
     print('Initializing camera object')
-    logging.info('Initializing camera object')
     gpio.output(SEL_1, False)
     gpio.output(SEL_2, False)
     time.sleep(0.2)
-    logging.info('Select pins OK')
+    print('Select pins OK')
     pi_cam = picamera.PiCamera()
     time.sleep(0.2)
-    logging.info('Cam init OK')
+    print('Cam init OK')
     pi_cam.resolution = resolution
-    logging.info('Resolution OK')
     print('Resolution OK')
     pi_cam.rotation = 180
     print('Rotation OK')
@@ -154,10 +110,10 @@ def initialize_background_play_pause():
 def initialize_logger(hike_num: int):
     # logname = 'log-hike' + str(hike_num) + '.log'
     logname = '/home/pi/capra-storage/logs/hike{n}.log'.format(n=hike_num)
-    print(logname, "LOGNAME")
     logging.basicConfig(filename=logname, level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-    #os.chmod(logname, 777)  # Make logfile accessible to writing by both root and user
+    # os.chmod(logname, 666) # Make logfile accessible to writing by both root and user
     logging.info('START')
+
 
 # Select camera + take a photo + save photo in file system and db
 def camcapture(pi_cam: picamera, cam_num: int, hike_num: int, photo_index: int, sql_controller: SQLController):
@@ -168,15 +124,15 @@ def camcapture(pi_cam: picamera, cam_num: int, hike_num: int, photo_index: int, 
         if cam_num == 1:
             gpio.output(SEL_1, True)
             gpio.output(SEL_2, False)
-            logging.info('cam 1 selected')
+            print("cam 1 selected")
         if cam_num == 2:
             gpio.output(SEL_1, False)
             gpio.output(SEL_2, False)
-            logging.info('cam 2 selected')
+            print("cam 2 selected")
         if cam_num == 3:
             gpio.output(SEL_1, True)
             gpio.output(SEL_2, True)
-            logging.info('cam 3 selected')
+            print("cam 3 selected")
         time.sleep(0.2)  # it takes some time for the pin selection
 
         # Build image file path
@@ -189,7 +145,8 @@ def camcapture(pi_cam: picamera, cam_num: int, hike_num: int, photo_index: int, 
         print('cam {c} -- picture taken!'.format(c=cam_num))
 
 
-# Tell altimeter to collect data; this process takes a while
+# Tell altimeter to collect data; this process (takes a while)
+# TODO - define "a while"
 def query_altimeter(bus: smbus):
     # MPL3115A2 address, 0x60(96) - Select control register, 0x26(38)
     # 0xB9(185)	Active mode, OSR = 128(0x80), Altimeter mode
@@ -208,69 +165,45 @@ def read_altimeter(bus: smbus) -> float:
 
 
 def main():
-    # Initialize PINs, LEDs, and GPIO logger
-    initialize_GPIOs()                              # Define the GPIO pin modes
-    hello_blinks()                                  # Say hello through LEDs
-    turn_off_leds()                                 # TODO - why do we need to
+    # Initialize and setup hardware
+    #initialize_GPIOs()                              # Define the GPIO pin modes
     i2c_bus = smbus.SMBus(1)                        # Setup I2C bus
+    turn_off_leds()                                 # TODO - why do we need to
+    hello_blinks()                                  # Say hello through LEDs
+    #pi_cam = initialize_picamera(RESOLUTION)        # Setup the camera
     initialize_background_play_pause()              # Setup play/pause button
     prev_pause = True
 
-    #initialize_picamera(RESOLUTION) # Setup the camera
-
     print('Initializing camera object')
-    logging.info('Initializing camera object')
     gpio.output(SEL_1, False)
     gpio.output(SEL_2, False)
     time.sleep(0.2)
-    logging.info('Select pins OK')
+    print('Select pins OK')
     pi_cam = picamera.PiCamera()
     time.sleep(0.2)
-    logging.info('Cam init OK')
-    pi_cam.resolution = resolution
-    logging.info('Resolution OK')
+    print('Cam init OK')
+    pi_cam.resolution = RESOLUTION
     print('Resolution OK')
     pi_cam.rotation = 180
     print('Rotation OK')
 
-    # TODO Integrate this into sql_controller to overwrite any existing database data
-    # and use the previous folder if it is empty.
-    # ALTERNATIVELY, move folder creation to after pause is taken out.
-    # FOLDER = '{d}hike{h}/'.format(d=DIRECTORY, h=hike_num)
-    # for dirpath, dirnames, files in os.walk(FOLDER):
-    #     if files:
-    #         print(dirpath, 'has files')
-    #     if not files:
-    #         print(dirpath, 'is empty')
-
 
     # Create SQL controller and update hike information
     sql_controller = SQLController(database=DB)
+
     created = sql_controller.will_create_new_hike(NEW_HIKE_TIME, DIRECTORY)
-    # <<<<<<< HEAD
-    #     if created:     # new hike created: blink green
-    #         blink(LED_GREEN, 2, 0.2)
-    #     else:           # continuing last hike: blink orange
-    #         blink(LED_AMBER, 2, 0.2)
-    # =======
     if created:     # new hike created; blink four times
         blink(LED_BTM, 4, 0.2)
-        #os.chmod(DIRECTORY, 777)  # set permissions to be read and written to when run manually
-        #os.chmod(DB, 777)
+        os.chmod(DIRECTORY, 666) # set permissions to be read and written to when run manually
+        os.chmod(DB , 666)
     else:           # continuing last hike; blink two times
         blink(LED_BTM, 2, 0.2)
     time.sleep(1)
     hike_num = sql_controller.get_last_hike_id()
-    print('HIKENO: ', hike_num, " =======")
     photo_index = sql_controller.get_last_photo_index_of_hike(hike_num)
 
-    # Initialize the logger for this specific hike
+    # Initialize logger
     initialize_logger(hike_num)
-
-    # Initialize camera and buttons
-    pi_cam = initialize_picamera(RESOLUTION)        # Setup the camera
-    initialize_background_play_pause()              # Setup play/pause button
-    prev_pause = True
 
     # Start the time lapse
     # --------------------------------------------------------------------------
@@ -286,7 +219,7 @@ def main():
         # If applicable, log 'unpaused'
         if(prev_pause):
             logging.info('Unpaused')
-            prev_pause = False
+            prev_pause= False
 
         # New picture: increment photo index & add row to database
         photo_index += 1
@@ -310,7 +243,7 @@ def main():
         if (photo_index % 4 == 0):
             blink(LED_GREEN, 1, 0.1)
             blink(LED_AMBER, 1, 0.1)
-            logging.info('Cameras OK')
+            logging.info('cameras still alive')
 
         # Wait until 2.5 seconds have passed since last picture
         while(time.time() < timestamp + 2.5):
