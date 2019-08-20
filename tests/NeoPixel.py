85f9a94609b7b6945d4d1298ae6b6e107ed8a0b8
Tal Amram <tal89amram@gmail.com>
2019-08-19 17:11:59 -0700

    Working Test

--- tests/NeoPixel.py
+++ tests/NeoPixel.py
@@ -9,7 +9,7 @@ import neopixel
 pixel_pin = board.D18
 
 # The number of NeoPixels
-num_pixels = 2
+num_pixels = 10
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
@@ -51,24 +51,24 @@ def rainbow_cycle(wait):
 
 while True:
     # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((255, 0, 0))
+#    pixels.fill((255, 0, 0))
     # Uncomment this line if you have RGBW/GRBW NeoPixels
     # pixels.fill((255, 0, 0, 0))
-    pixels.show()
-    time.sleep(1)
+#    pixels.show()
+#    time.sleep(1)
 
     # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 255, 0))
+#    pixels.fill((0, 255, 0))
     # Uncomment this line if you have RGBW/GRBW NeoPixels
     # pixels.fill((0, 255, 0, 0))
-    pixels.show()
-    time.sleep(1)
+#    pixels.show()
+#    time.sleep(1)
 
     # Comment this line out if you have RGBW/GRBW NeoPixels
-    pixels.fill((0, 0, 255))
+#    pixels.fill((0, 0, 255))
     # Uncomment this line if you have RGBW/GRBW NeoPixels
     # pixels.fill((0, 0, 255, 0))
-    pixels.show()
-    time.sleep(1)
+#    pixels.show()
+#    time.sleep(1)
 
     rainbow_cycle(0.001)    # rainbow cycle with 1ms delay per step
