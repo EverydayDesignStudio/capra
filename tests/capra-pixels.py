ad0a353d40fdf16d934cffb5fc00b93a4876c465
Tal Amram <tal89amram@gmail.com>
2019-08-19 17:11:40 -0700

    Test

--- tests/capra-pixels.py
+++ tests/capra-pixels.py
@@ -1,20 +1,24 @@
 import RPi.GPIO as gpio
 import board
 import neopixel
+import time
 
 PIXEL_AMOUNT = 11       # The amount of NeoPixels on the strip
+ORDER = neopixel.RGB
+PIXEL_PIN = board.D18
 
-gpio.setup(board.D18, gpio.OUT)
+pixels = neopixel.NeoPixel(PIXEL_PIN, PIXEL_AMOUNT, brightness=0.2, auto_write=False, pixel_order=ORDER)
+pos = 0
 
-gpio.output(board.D18, True)
+def indicator1(position: float):
+    brights = position - 1
+    for p in range(int(brights):
+        pixels[p] = (255, 255, 255)
+    intensity = 255 * (position - brights)
+    pixels[brights + 1](intensity, intensity, intensity)
 
-#
-# pixels = neopixel.NeoPixel(board.D18, PIXEL_AMOUNT)
-# pixels.fill((0, 255, 0))
-#
-# def indicator1(position: float):
-#     brights = position - 1
-#     for p in range(int(brights):
-#         pixels[p] = (255, 255, 255)
-#     intensity = 255 * (position - brights)
-#     pixels[brights + 1](intensity, intensity, intensity)
+
+while(1):
+    pos += 0.01
+    indicator(pos)
+    time.sleep(0.1)
