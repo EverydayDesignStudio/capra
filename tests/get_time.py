2ea6571735bd88bd98a627ad00ea75901310e6e3
Jordan White <jordanroywhite@gmail.com>
2019-10-10 20:22:30 -0700

    Add python3 env line

--- tests/get_time.py
+++ tests/get_time.py
@@ -1,5 +1,6 @@
-# Script for checking the set time of DS3231 Real time clock
+#!/usr/bin/env python3
 
+# Script for checking the set time of DS3231 Real time clock
 import time                  # For unix timestamps
 import busio                 # For interfacing with DS3231 Real Time Clock
 import adafruit_ds3231       # pip install adafruit-circuitpython-ds3231
