61d6a1b255958c9b1149f762b5b148ce3648a781
Jordan White <jordan_white@sfu.ca>
2019-10-10 22:05:06 -0700

    Add setup script to set DS3231 RTC with current time

--- tests/get_time.py
+++ tests/get_time.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python3
 
 # Script for checking the set time of DS3231 Real time clock
-import time                  # For unix timestamps
-import busio                 # For interfacing with DS3231 Real Time Clock
-import adafruit_ds3231       # pip install adafruit-circuitpython-ds3231
+import time                     # For unix timestamps
+from datetime import datetime   # For printing readable time
+import busio                    # For interfacing with DS3231 Real Time Clock
+import adafruit_ds3231          # pip install adafruit-circuitpython-ds3231
 
 
-i2c = busio.I2C(3, 2)
-timestamp = get_RTC_time(i2c)
-print("TIME SET TO: ")
-print(timestamp)
+def main():
+    i2c = busio.I2C(3, 2)
+    timestamp = get_RTC_time(i2c)
+    print("TIME SET TO: ")
+    print(timestamp)
+    print(datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S'))
 
 
 def get_RTC_time(I2C):
     rtc = adafruit_ds3231.DS3231(I2C)
     t = time.mktime(rtc.datetime)
     return(t)
+
+
+if __name__ == "__main__":
+    main()
