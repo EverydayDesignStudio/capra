68f2e6042befdc60268d28d9e28bbceed8de320e
Jordan White <jordanroywhite@gmail.com>
2019-10-11 17:05:55 -0700

    Rename and move SQL build scripts and functions

--- setup/db_build_script_projector.sql
+++ setup/db_build_script_projector.sql
@@ -1,44 +1,6 @@
 ----------------------------------------------------------------
--- CAMERA BUILD
-CREATE DATABASE capra_camera.db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE capra_camera.db;
-
-DROP TABLE IF EXISTS "pictures";
-
--- no picture_id, this is added upon transfer back to projector
-CREATE TABLE "pictures" (
-	"time"	REAL UNIQUE,
-	"altitude"	REAL,
-	"color"	TEXT,
-	"hike"	INTEGER,
-	"index_in_hike"	INTEGER,
-	"camera1"	TEXT UNIQUE,
-	"camera2"	TEXT UNIQUE,
-	"camera3"	TEXT UNIQUE,
-	"created_date_time" TEXT DEFAULT CURRENT_TIMESTAMP,
-	"updated_date_time" TEXT DEFAULT CURRENT_TIMESTAMP,
-	FOREIGN KEY("hike") REFERENCES "hikes"("hike_id")
-);
-
-DROP TABLE IF EXISTS "hikes";
-
-CREATE TABLE "hikes" (
-	"hike_id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"average_altitude"	REAL,
-	"average_color"	TEXT,
-	"start_time"	REAL UNIQUE,
-	"end_time"	REAL UNIQUE,
-	"pictures"	INTEGER,
-	"path" TEXT UNIQUE,
-	"created_date_time" TEXT DEFAULT CURRENT_TIMESTAMP,
-	"updated_date_time" TEXT DEFAULT CURRENT_TIMESTAMP
-);
-
-.save capra_camera.db;
-
-
+-- PROJECTOR (EXPLORER) BUILD SCRIPT
 ----------------------------------------------------------------
--- PROJECTOR BUILD
 CREATE DATABASE capra_projector.db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE capra_projector.db;
 
