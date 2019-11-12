365d35f8cda9238a71c0d8af79ae474d86c6bb1a
Jordan White <jordan_white@sfu.ca>
2019-11-11 18:49:46 -0800

    Add logic to move newly created db

--- setup/create_db_camera.py
+++ setup/create_db_camera.py
@@ -5,8 +5,12 @@
 import os
 import sqlite3
 
-# Builds from the absolute path of the file regardless from where it is called
+
+# Builds from the absolute path of the file
+# regardless from where this scripts is called
 path = os.path.dirname(os.path.abspath(__file__))
+
+# Create path for db creation buildscript
 file = 'db_build_script_camera.sql'
 build_script_path = '{p}/{f}'.format(p=path, f=file)
 
@@ -14,10 +18,24 @@ build_script_path = '{p}/{f}'.format(p=path, f=file)
 with open(build_script_path, 'r') as sql_file:
     sql_build_script = sql_file.read()
 
-# Run the command
+# Run the SQL command
 db = sqlite3.connect('capra_camera.db')
 cursor = db.cursor()
 cursor.executescript(sql_build_script)
 print('Building new Capra Collector camera database...')
 db.commit()
 db.close()
+
+# Create a new directory for capra-storage
+storage_path = '{p}/../../capra-storage'.format(p=path)
+if not os.path.exists(storage_path):
+    os.makedirs(storage_path)
+    print('✅ SUCCESS: directory created @ {sp}'.format(sp=storage_path))
+else:
+    print('⚠️ WARNING: directory already exists; no directory created')
+
+# Move the newly created database file to capra-storage
+new_db_path = '{sp}/capra_camera.db'.format(sp=storage_path)
+os.rename('capra_camera.db', new_db_path)
+
+print('✅ SUCCESS: database created @ {dbp}'.format(dbp=new_db_path))
