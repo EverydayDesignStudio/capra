5370eccecfb8bfa5ad474e3e666c62106ba707f0
Minyoung Yoo <yoomy1203@hotmail.com>
2019-11-20 17:58:20 -0800

    Test rsync on each hike with dummy data

--- capra-transfer.py
+++ capra-transfer.py
@@ -22,6 +22,7 @@
 import globals as g
 import os, datetime
 from pathlib import Path
+import subprocess
 
 rsync_status = None;
 cur_masterDB = None;
@@ -29,10 +30,10 @@ cur_remoteDB = None;
 
 def getDBCursor(dbPath):
     conn = sqlite3.connect(dbPath);
-    cur = conn.cursor()
+    cur = conn.cursor();
     return cur;
 
-def transfer_from_camera(from, to):
+def transfer_from_camera(src, dest):
     # TODO: write this subprocess call
     rsync_status = deploy_subprocess;
     return;
@@ -47,18 +48,25 @@ def update_transfer_animation_db():
     if date.today() != date.fromtimestamp(Path(g.PATH_TRANSFER_ANIMATION_DB).stat().st_mtime):
         transfer_from_camera(g.PATH_CAMERA_DB, g.PATH_TRANSFER_ANIMATION_DB);
 
-def build_hike_path(base, hikeID):
-    return 'C:\\Users\\Minyoung Yoo\\Dropbox\\_SFU\\7. RA\\capra-sample-data\\capra-storage' + 'hike' + hikeID;
+def build_hike_path(base, hikeID=None, makeNew=False):
+    res = "";
+    if (hikeID > 0):
+        res = '../capra-sample-data/' + base + '/hike' + str(hikeID);
+    else:
+        res = '../capra-sample-data/' + base;
+    if makeNew and not os.path.exists(res):
+        os.makedirs(res)
+    return res;
     # return base + 'hike' + hikeID;
 
 def build_picture_path(base, hikeID, index, camNum):
-    return 'C:\\Users\\Minyoung Yoo\\Dropbox\\_SFU\\7. RA\\capra-sample-data\\capra-storage' + 'hike' + hikeID
-    return base + 'hike' + hikeID + '/' + index + '_cam' + camNum + '.jpg';
+    return '../capra-sample-data/' + base + '/hike' + str(hikeID) + '/' + str(index) + '_cam' + str(camNum) + '.jpg';
+    # return base + 'hike' + str(hikeID) + '/' + str(index) + '_cam' + str(camNum) + '.jpg';
 
 def start_transfer():
     # TODO: write query
-    latest_master_hikeID = 0;
-    latest_remote_hikeID = 0;
+    latest_master_hikeID = 1; ##
+    latest_remote_hikeID = 3; ##
     numNewHikes = latest_remote_hikeID - latest_master_hikeID;
     hikeCounter = 1;
     checkSum = 0;
@@ -66,11 +74,16 @@ def start_transfer():
     # 3. determine how many hikes should be transferred
     while hikeCounter <= numNewHikes:
         # TODO: write this query; SELECT pictures FROM hikes WHERE hike_id = (latest_master_hikeID + hikeCounter);
-        checkSum = getNumCountFromHike;
+        checkSum = 0;
+        if (hikeCounter == 1):
+            checkSum = 100; # getNumCountFromHike;
+        elif (hikeCounter == 2):
+            checkSum = 153; # getNumCountFromHike;
         currHike = latest_master_hikeID + hikeCounter;
 
         # TODO: deploy rsync subprocess
-        rsync_status = deploy_rsync_subprocess;
+        rsync_status = subprocess.call(['rsync', '-av', build_hike_path("capra-hd", currHike), build_hike_path("capra-storage", -1, True)]); # deploy_rsync_subprocess;
+
 
         # 4. while a hike is being transferred, find newly added file and write a row to the master DB
         # 5. once a hike is fully transferred, compare the checksum
@@ -79,29 +92,49 @@ def start_transfer():
         picCounter_cam2 = 0;
         picCounter_cam3 = 0;
         picTotal = 0;
-        while (rsync_status is None and os.path.exists(build_hike_path(g.PATH_ON_PROJECTOR, currHike)) and picTotal < checkSum):
-            if (os.path.exists(build_picture_path(g.PATH_ON_PROJECTOR, currHike, picCounter_cam1, 1)):
+        # rsync_status is None and
+        # try:
+        # while (os.path.exists(build_hike_path(g.PATH_ON_PROJECTOR, currHike)) and picTotal < checkSum):
+        abort = 0;
+
+        while (os.path.exists(build_hike_path("capra-storage", currHike)) and picTotal < checkSum):
+#             if (os.path.exists(build_picture_path(g.PATH_ON_PROJECTOR, currHike, picCounter_cam1, 1)):
+            if (os.path.exists(build_picture_path("capra-storage", currHike, picCounter_cam1, 1))):
                 # TODO: make query and
                 picCounter_cam1 += 1;
-            if (os.path.exists(build_picture_path(g.PATH_ON_PROJECTOR, currHike, picCounter_cam2, 2)):
+                #print("Cam1++; Cam1 total: {}".format(picCounter_cam1));
+            # if (os.path.exists(build_picture_path(g.PATH_ON_PROJECTOR, currHike, picCounter_cam2, 2)):
+            if (os.path.exists(build_picture_path("capra-storage", currHike, picCounter_cam2, 2))):
                 # TODO: make query and
                 picCounter_cam2 += 1;
-            if (os.path.exists(build_picture_path(g.PATH_ON_PROJECTOR, currHike, picCounter_cam3, 3)):
+                #print("Cam2++; Cam2 total: {}".format(picCounter_cam2));
+            # if (os.path.exists(build_picture_path(g.PATH_ON_PROJECTOR, currHike, picCounter_cam3, 3)):
+            if (os.path.exists(build_picture_path("capra-storage", currHike, picCounter_cam3, 3))):
                 # TODO: make query and
                 picCounter_cam3 += 1;
+                #print("Cam3++; Cam3 total: {}".format(picCounter_cam3));
 
             # TODO: set timeout to be.. 5 min?
-            if (set_timeout_flag):
+            # if (set_timeout_flag):
+            if (False):
                 abort_rsync_subprocess
                 break;
 
-            if (prevPicTotal != picTotal):
-                prevPicTotal = picTotal;
-                picTotal = picCounter_cam1 + picCounter_cam2 + picCounter_cam3;
+            tmpTotal = picCounter_cam1 + picCounter_cam2 + picCounter_cam3;
+            if (prevPicTotal < tmpTotal):
+                prevPicTotal = tmpTotal;
+                picTotal = prevPicTotal;
+            else:
+                abort += 1;
+
+            if (abort >= 10):
+                exit();
+        print("Hike{} Total: {}".format(currHike, picTotal));
 
         # 5-1. restore/re-transfer any missing picture
         # TODO: handle timeout; go through the folder again and re-transfer missing file
-        if (rsync_status != 0 or timeout is True or picTotal != checkSum):
+        # >> maybe add a dummy black photo to replace the missing file
+        # if (rsync_status != 0 or timeout is True or picTotal != checkSum):
 
 
         # reset variables before the next iteration
