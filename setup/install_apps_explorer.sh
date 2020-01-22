b0ba75207111f7cde1e802930ce5301af619682a
Jordan White <jordan_white@sfu.ca>
2020-01-22 15:22:43 -0800

    Add dependencies

--- setup/install_apps_explorer.sh
+++ setup/install_apps_explorer.sh
@@ -5,3 +5,4 @@ apt-get update
 apt-get install vim --assume-yes
 apt-get install python3-pil python3-pil.imagetk --assume-yes
 apt-get install exfat-fuse exfat-utils --assume-yes
+apt-get install i2c-tools
