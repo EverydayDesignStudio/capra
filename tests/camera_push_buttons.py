b7e3e61d098d501e79c6d79a305dd74d08e83c08
Jordan White <jordan_white@sfu.ca>
2020-05-27 00:11:48 -0700

    Add tests for cameras, LED, buttons

---
+++ tests/camera_push_buttons.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import RPi.GPIO as GPIO
+import time
+import sys
+
+PLAY_PAUSE = 17
+OFF = 25
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PLAY_PAUSE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(OFF, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def write(line):
+    sys.stdout.write(line)
+    sys.stdout.flush()
+
+write("--- Push Button Values ---")
+
+while True:
+    input_play_pause = GPIO.input(PLAY_PAUSE)
+    if input_play_pause == True:
+        status_pp = 'Button is pressed'
+    else:
+        status_pp = 'Button is not pressed'
+
+    input_off = GPIO.input(OFF)
+    if input_off == True:
+        status_off = 'Button is pressed'
+    else:
+        status_off = 'Button is not pressed'
+
+    time.sleep(0.05)
+
+    output = """
+Play/Pause Button: {pp}
+Off Button: {off}
+""".format(pp=status_pp, off=status_off)
+
+    output = output.replace("\n", "\n\033[K")
+    write(output)
+    lines = len(output.split("\n"))
+    write("\033[{}A".format(lines - 1))
