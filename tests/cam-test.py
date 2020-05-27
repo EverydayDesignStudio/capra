73a88798bb2773ab91e49e494c3344ddecda8f63
Jordan White <jordan_white@sfu.ca>
2020-05-26 18:44:29 -0700

    Remove extra code for LEDs

--- tests/cam-test.py
+++ tests/cam-test.py
@@ -1,44 +1,19 @@
 #!/usr/bin/env python3
 
-import time  # For time keeping
-import picamera  # For interfacting with the PiCamera
-import RPi.GPIO as gpio  # For
-
+import time             # Time keeping
+import picamera         # Interfacting with PiCamera
+import RPi.GPIO as gpio # Interfacing with IO 
 
 SEL_1 = 22
 SEL_2 = 23
-LED_GREEN = 24
-LED_BTM = 26
-LED_AMBER = 27
 
 # Initialize GPIO pins
 gpio.setmode(gpio.BCM)
+gpio.setwarnings(False)
 gpio.setup(SEL_1, gpio.OUT)  # select 1
 gpio.setup(SEL_2, gpio.OUT)  # select 2
-gpio.setup(LED_GREEN, gpio.OUT)  # status led1
-gpio.setup(LED_AMBER, gpio.OUT)  # status led2
-gpio.setup(LED_BTM, gpio.OUT)  # status led3
-
-# Turn off LEDs
-gpio.output(LED_GREEN, True)
-time.sleep(0.1)
-gpio.output(LED_AMBER, True)
-time.sleep(0.1)
-gpio.output(LED_BTM, False)
 
 RESOLUTION = (1280, 720)
-
-def blink(pin, repeat, interval):
-    on = False
-    off = True
-    if pin == LED_BTM:
-        on = True
-        off = False
-    for i in range(repeat):
-        gpio.output(pin, on)
-        time.sleep(interval)
-        gpio.output(pin, off)
-        time.sleep(interval)
 
 # For Selecting Cam and taking + saving a picture
 def camcapture(_cam, _camno):
@@ -63,14 +38,9 @@ def camcapture(_cam, _camno):
         photoname = dir + 'cam' + str(_camno) + '.jpg'
         print(photoname)
         _cam.capture(photoname)
-        print('cam', str(_camno), '- picture taken!')
+        print('✅ cam', str(_camno), '- picture taken!')
 
 def main():
-    # Hello blinks
-    blink(LED_GREEN, 2, 0.1)
-    blink(LED_AMBER, 2, 0.1)
-    blink(LED_BTM, 2, 0.1)
-
     # Initialize camera object
     print('initializing camera')
     gpio.output(SEL_1, False)
@@ -79,10 +49,11 @@ def main():
     cam = picamera.PiCamera()
     cam.resolution = RESOLUTION
 
-    # take three pictures
+    # Take three pictures
     camcapture(cam, 1)
     camcapture(cam, 2)
     camcapture(cam, 3)
+    print('\n🎉 Your cameras are connected properly!')
 
 if __name__ == "__main__":
     main()
