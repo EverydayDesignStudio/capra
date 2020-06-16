7e81a53021fa37e51dbdf07d939677f960ea2296
Jordan White <jordan_white@sfu.ca>
2020-06-16 15:53:16 -0700

    Add updated time in SQL call for updating altitude

--- classes/sql_statements.py
+++ classes/sql_statements.py
@@ -227,8 +227,8 @@ class SQLStatements:
         return statement
 
     def update_picture_altitude(self, altitude: float, rowid: int) -> str:
-        statement = 'UPDATE  pictures SET altitude={a} WHERE ROWID={r} \
-            '.format(a=altitude, r=rowid)
+        statement = 'UPDATE  pictures SET altitude={a}, updated_date_time=datetime() \
+            WHERE ROWID={r}'.format(a=altitude, r=rowid)
         return statement
 
     def update_hike_path(self, path: str, hike_id: int) -> str:
