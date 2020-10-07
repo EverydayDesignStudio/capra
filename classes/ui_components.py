3da53f1e7da263055ae8b6b5c88c2ee57eb73324
Jordan White <jordanroywhite@gmail.com>
2020-10-07 14:30:24 -0700

    Add initial ModeOverlay component

--- classes/ui_components.py
+++ classes/ui_components.py
@@ -2,11 +2,15 @@
 
 # Various helper UI classes for easier development of PyQt5 applications
 
+# Imports
+# from projector_slideshow import *
+
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
 import sys
+import time
 
 
 # Add a single color widget, great for testing purposes
@@ -38,3 +42,77 @@ class ImageOverlay(QLabel):
         self.setAlignment(Qt.AlignCenter)
         pixmap = QPixmap(path)
         self.setPixmap(pixmap)
+
+
+class ModeOverlay(QLabel):
+    def __init__(self, window, path: str, mode, *args, **kwargs):
+        super(QLabel, self).__init__(window, *args, **kwargs)
+        # QLabel.__init__(self, window)
+
+        self.resize(1280, 720)
+        # self.move(0, 0)
+        self.setAlignment(Qt.AlignCenter)
+        # self.setStyleSheet("border: 3px solid pink; background-color: rgba(0, 0, 0, 100);")
+        self.setStyleSheet("background-color: rgba(0, 0, 0, 50);")
+
+        # TODO - remove this because its so janky
+        fadeEffect = QGraphicsOpacityEffect()
+        self.setGraphicsEffect(fadeEffect)
+        anim1 = QPropertyAnimation(fadeEffect, b"opacity")
+        anim1.setEasingCurve(QEasingCurve.InCubic)
+        anim1.setStartValue(0)
+        anim1.setEndValue(0)
+        anim1.setDuration(50)
+        anim1.start()
+
+        # pixmap = QPixmap('assets/Time@1x.png')
+        # self.setPixmap(pixmap)
+
+        # self.hide()
+
+    def setTime(self):
+        print('time')
+        pixmap = QPixmap('assets/Time@1x.png')
+        self.setPixmap(pixmap)
+        self._doAnimation()
+
+    def setAltitude(self):
+        print('alt')
+        pixmap = QPixmap('assets/Altitude@1x.png')
+        self.setPixmap(pixmap)
+        self._doAnimation()
+
+    def setColor(self):
+        print('color')
+        pixmap = QPixmap('assets/Color@1x.png')
+        self.setPixmap(pixmap)
+        self._doAnimation()
+
+    def _doAnimation(self):
+        fadeEffect = QGraphicsOpacityEffect()
+        self.setGraphicsEffect(fadeEffect)
+
+        anim1 = QPropertyAnimation(fadeEffect, b"opacity")
+        anim1.setEasingCurve(QEasingCurve.InCubic)
+        anim1.setStartValue(0)
+        anim1.setEndValue(1)
+        anim1.setDuration(750)
+
+        anim2 = QPropertyAnimation(fadeEffect, b"opacity")
+        anim2.setEasingCurve(QEasingCurve.OutCubic)
+        anim2.setStartValue(1)
+        anim2.setEndValue(0)
+        anim2.setDuration(750)
+
+        group = QSequentialAnimationGroup(self)
+        group.addAnimation(anim1)
+        group.addPause(2000)
+        group.addAnimation(anim2)
+        # group.addAnimation(anim1)
+        group.start()
+
+        # self.anim = QPropertyAnimation(self.label, b"geometry")
+        # self.anim.setDuration(400)
+        # self.anim.setStartValue(QRect(0, 720, 1280, 110))
+        # self.anim.setEndValue(QRect(0, 610, 1280, 110))
+        # self.anim.start()
