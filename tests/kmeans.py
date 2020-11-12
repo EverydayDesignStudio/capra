6b06be22adeeb0e914de1d331a3eb2b89b631bdf
MinYoung Yoo <yoomy1203@hotmail.com>
2020-11-12 09:17:13 -0800

    Implement color converting from HSV to RGB in the test script

--- tests/kmeans.py
+++ tests/kmeans.py
@@ -4,11 +4,15 @@ import numpy as np
 import cv2  # pip install opencv-python : for resizing image
 from pathlib import Path
 import time
+import colorsys
+from colormath.color_objects import HSVColor, LabColor
+from colormath.color_conversions import convert_color
+from colormath.color_diff import delta_e_cie2000
 
 DATAPATH = '/Users/myoo/Development/capra_data/_sample/hike0/'
-CLUSTERS = 5
-DIMX = 720
-DIMY = 427
+CLUSTERS = 10
+DIMX = 160
+DIMY = 95
 TOTAL = DIMX * DIMY
 OUTPUTPATH = DATAPATH + "dominantColors_" + str(DIMX) + "x" + str(DIMY) + '_' + str(CLUSTERS) + ".txt"
 
@@ -49,7 +53,7 @@ def get_dominant_color(image, k=4, image_processing_size=None):
     return list(dominant_color)
 
 
-def get_multiple_dominant_colors(image, k, image_processing_size=None):
+def get_multiple_dominant_colors(image1, image2, image3, k, image_processing_size=None):
     """
     takes an image as input
     returns two lists
@@ -78,10 +82,16 @@ def get_multiple_dominant_colors(image, k, image_processing_size=None):
 
     # resize image if new dims provided
     if image_processing_size is not None:
-        image = cv2.resize(image, image_processing_size, interpolation=cv2.INTER_AREA)
+        image1 = cv2.resize(image1, image_processing_size, interpolation=cv2.INTER_AREA)
+        image2 = cv2.resize(image2, image_processing_size, interpolation=cv2.INTER_AREA)
+        image3 = cv2.resize(image3, image_processing_size, interpolation=cv2.INTER_AREA)
 
     # reshape the image to be a list of pixels
-    image = image.reshape((image.shape[0] * image.shape[1], 3))
+    image1 = image1.reshape((image1.shape[0] * image1.shape[1], 3))
+    image2 = image2.reshape((image2.shape[0] * image2.shape[1], 3))
+    image3 = image3.reshape((image3.shape[0] * image3.shape[1], 3))
+
+    image = np.concatenate((image1, image2, image3))
 
     # cluster and assign labels to the pixels
     clt = KMeans(n_clusters=k)
@@ -90,16 +100,69 @@ def get_multiple_dominant_colors(image, k, image_processing_size=None):
     # count labels to find most popular
     label_counts = Counter(labels)
 
-    # subset out 5 popular centroids
-    top5 = label_counts.most_common(CLUSTERS)
-    dominant_colors = []
+    # subset out k popular centroids
+    topK = label_counts.most_common(k)
+    dominant_colors_hsv = []
     confidences = []
-    for label in top5:
+
+    for label in topK:
         tmp = [ int(e) for e in clt.cluster_centers_[label[0]].tolist() ]
-        dominant_colors.append(tmp)
+        dominant_colors_hsv.append(tmp)
         confidences.append(round(label[1]/TOTAL, 2))
 
-    return dominant_colors, confidences
+    print(dominant_colors_hsv)
+    print(confidences)
+
+    # exclude colors that are similar to existing ones and with < 0.1 confidence value
+    index_to_remove = set()
+
+    for i in range(k):
+        color_i = dominant_colors_hsv[i]
+        color_i_hsv = HSVColor(color_i[0], color_i[1], color_i[2])
+        color_i_lab = convert_color(color_i_hsv, LabColor)
+
+        color_i_delta = []
+
+        for j in range(i+1, k):
+            color_j = dominant_colors_hsv[j]
+            color_j_hsv = HSVColor(color_j[0], color_j[1], color_j[2])
+            color_j_lab = convert_color(color_j_hsv, LabColor)
+
+            delta_e = delta_e_cie2000(color_i_lab, color_j_lab);
+
+            color_i_delta.append(delta_e)
+
+        for j in range (len(color_i_delta)):
+            if (confidences[i+j+1] < 0.2 and color_i_delta[j] < 40):
+                index_to_remove.add(i+j+1)
+
+        # print("color {} ({}) : \n\t{}".format(i, color_i, color_i_delta))
+        # print("index to remove : {}".format(index_to_remove))
+
+    itr = list(index_to_remove)
+    itr.sort(reverse = True)
+    print("ITR : {}".format(itr))
+
+    # trim insignificant colors
+    for i in range(len(itr)):
+        dominant_colors_hsv.pop(itr[i])
+        confidences.pop(itr[i])
+
+    dominant_colors_rgb = []
+    for color_hsv in dominant_colors_hsv:
+        color_rgb = colorsys.hsv_to_rgb(color_hsv[0]/180., color_hsv[1]/255., color_hsv[2]/255.)
+        # print(list(color_rgb))
+        ret = list(map(lambda a : round(a*255), list(color_rgb)))
+        # print(ret)
+        dominant_colors_rgb.append(ret)
+
+    print("\n")
+    print(dominant_colors_hsv)
+    print(dominant_colors_rgb)
+    print(confidences)
+
+
+    return len(confidences), dominant_colors_hsv, dominant_colors_rgb, confidences
 
 
 startTime = time.time()
@@ -108,7 +171,7 @@ pathlist = Path(DATAPATH).glob('*.jpg')
 img = ""
 res = []
 count = 1
-for path in pathlist:
+# for path in pathlist:
     # if (count < 1000):
     #     count += 1
     #     continue
@@ -117,64 +180,93 @@ for path in pathlist:
 
     # try:
     # because path is object not string
-    img = str(path)
-    imgName = img.rsplit('/', 1)[1]
-    # print("{} - {}".format(count, imgName))
+img1 = str("/Users/myoo/Development/capra_data/_sample/hike0/194_cam1.jpg")
+img2 = str("/Users/myoo/Development/capra_data/_sample/hike0/194_cam2.jpg")
+img3 = str("/Users/myoo/Development/capra_data/_sample/hike0/194_cam3.jpg")
+# imgName = img.rsplit('/', 1)[1]
+# print("{} - {}".format(count, imgName))
 
-    # read in image of interest
-    bgr_image = cv2.imread(img)
-    # convert to HSV; this is a better representation of how we see color
-    hsv_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
+# read in image of interest
+bgr_image1 = cv2.imread(img1)
+bgr_image2 = cv2.imread(img2)
+bgr_image3 = cv2.imread(img3)
+# convert to HSV; this is a better representation of how we see color
+hsv_image1 = cv2.cvtColor(bgr_image1, cv2.COLOR_BGR2HSV)
+hsv_image2 = cv2.cvtColor(bgr_image2, cv2.COLOR_BGR2HSV)
+hsv_image3 = cv2.cvtColor(bgr_image3, cv2.COLOR_BGR2HSV)
 
-    # # extract dominant color (in HSV)
-    # # (aka the centroid of the most popular k means cluster)
-    # dom_color_hsv = get_dominant_color(hsv_image, k=CLUSTERS, image_processing_size=(DIMX, DIMY))
-    #
-    # # temporarily create a pixel with HSV to extract RGB value
-    # tmp = np.full((1, 1, 3), dom_color_hsv, dtype='uint8')
-    # dom_color_rgb = cv2.cvtColor(tmp, cv2.COLOR_HSV2RGB)[0][0].tostring()
-    #
-    # # convert lists to string
-    # dom_color_hsv_str = ', '.join(map(str, dom_color_hsv))
-    # dom_color_rgb_str = ', '.join(map(str, dom_color_rgb))
+# # extract dominant color (in HSV)
+# # (aka the centroid of the most popular k means cluster)
+# dom_color_hsv = get_dominant_color(hsv_image, k=CLUSTERS, image_processing_size=(DIMX, DIMY))
+#
+# # temporarily create a pixel with HSV to extract RGB value
+# tmp = np.full((1, 1, 3), dom_color_hsv, dtype='uint8')
+# dom_color_rgb = cv2.cvtColor(tmp, cv2.COLOR_HSV2RGB)[0][0].tostring()
+#
+# # convert lists to string
+# dom_color_hsv_str = ', '.join(map(str, dom_color_hsv))
+# dom_color_rgb_str = ', '.join(map(str, dom_color_rgb))
 
-    dom_colors_hsv, conf_list= get_multiple_dominant_colors(hsv_image, k=CLUSTERS, image_processing_size=(DIMX, DIMY))
+color_size, dom_colors_hsv, dom_colors_rgb, conf_list= get_multiple_dominant_colors(hsv_image1, hsv_image2, hsv_image3, k=CLUSTERS, image_processing_size=(DIMX, DIMY))
 
-    res_rgb = []
-    res_hsv = []
-    for dom_color_hsv in dom_colors_hsv:
-        # temporarily create a pixel with HSV to extract RGB value
-        tmp = np.full((1, 1, 3), dom_color_hsv, dtype='uint8')
-        dom_color_rgb = cv2.cvtColor(tmp, cv2.COLOR_HSV2RGB)[0][0].tostring()
+res_hsv = ""
+res_rgb = ""
+conf = ""
 
-        # convert lists to string
-        dom_color_hsv_str = ', '.join(map(str, dom_color_hsv))
-        dom_color_rgb_str = ', '.join(map(str, dom_color_rgb))
+for i in range(color_size):
+    add = ","
+    if (i == color_size-1):
+        add = ""
 
-        res_hsv.append(dom_color_hsv_str)
-        res_rgb.append(dom_color_rgb_str)
+    res_hsv += str(dom_colors_hsv[i][0]) + "," + str(dom_colors_hsv[i][1]) + "," + str(dom_colors_hsv[i][2]) + add
+    res_rgb += str(dom_colors_rgb[i][0]) + "," + str(dom_colors_rgb[i][1]) + "," + str(dom_colors_rgb[i][2]) + add
+    conf += str(conf_list[i]) + add
 
-    row = imgName + ', ' + ', '.join(res_hsv) + ', ' + ', '.join(res_rgb)
 
-    res = "{} ({})\n".format(imgName, count)
-    res += "\t{}\n".format(res_hsv)
-    res += "\t{}\n".format(res_rgb)
-    res += "\t{}".format(conf_list)
 
-    print("{} ({})".format(imgName, count))
-    print("\t{}".format(res_hsv))
-    print("\t{}".format(res_rgb))
-    print("\t{}".format(conf_list))
+print("\n")
+print(res_hsv)
+print(res_rgb)
+print(conf)
 
-    # res.append(row)
+#
+# res_rgb = []
+# res_hsv = []
+# for dom_color_hsv in dom_colors_hsv:
+#     # temporarily create a pixel with HSV to extract RGB value
+#     tmp = np.full((1, 1, 3), dom_color_hsv, dtype='uint8')
+#     dom_color_rgb = cv2.cvtColor(tmp, cv2.COLOR_HSV2RGB)[0][0].tostring()
+#
+#     # convert lists to string
+#     dom_color_hsv_str = ', '.join(map(str, dom_color_hsv))
+#     dom_color_rgb_str = ', '.join(map(str, dom_color_rgb))
+#
+#     res_hsv.append(dom_color_hsv_str)
+#     res_rgb.append(dom_color_rgb_str)
 
-    count += 1
-    # except:
-    #     print(count)
+#
+# row = imgName + ', ' + ', '.join(res_hsv) + ', ' + ', '.join(res_rgb)
+#
+# res = "{} ({})\n".format(imgName, count)
+# res += "\t{}\n".format(res_hsv)
+# res += "\t{}\n".format(res_rgb)
+# res += "\t{}".format(conf_list)
+#
+# print("{} ({})".format(imgName, count))
+# print("\t{}".format(res_hsv))
+# print("\t{}".format(res_rgb))
+# print("\t{}".format(conf_list))
+#
+# # res.append(row)
+#
+# count += 1
+#     # except:
+#     #     print(count)
+#
+# with open(OUTPUTPATH, 'w') as filehandle:
+#     for row in res:
+#         filehandle.write('%s\n' % row)
 
-with open(OUTPUTPATH, 'w') as filehandle:
-    for row in res:
-        filehandle.write('%s\n' % row)
 
 endTime = time.time()
 print("### {} photos analyzed.".format(str(count)))
