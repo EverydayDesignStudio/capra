c25e3625aaca452269d1cf3a342e178d6a960af8
Min <yoomy1203@hotmail.com>
2021-03-09 01:35:46 -0800

    Retract UNIQUE constraints for rank columns due to conflict on update

--- setup/db_build_script_projector.sql
+++ setup/db_build_script_projector.sql
@@ -15,17 +15,17 @@ CREATE TABLE "pictures" (
 	"dayofweek"						INTEGER,
 	"hike"								INTEGER,
 	"index_in_hike"				INTEGER,
-	"time_rank_global"		INTEGER UNIQUE,	--  first sorted by minute, then time
+	"time_rank_global"		INTEGER,				--  *UNIQUE* first sorted by minute, then time
 	"altitude"						REAL,
 	"altrank_hike"	  		INTEGER, 				--
-	"altrank_global"	  	INTEGER UNIQUE, --
-	"altrank_global_h"		INTEGER UNIQUE, --
+	"altrank_global"	  	INTEGER, 				--  *UNIQUE*
+	"altrank_global_h"		INTEGER, 				--  *UNIQUE*
 	"color_hsv"						TEXT,						-- "H,S,V" (decimal)
 	"color_rgb"						TEXT,						-- "R,G,B" (int)
 	"color_rank_value"		TEXT,						-- possible intermediate value used to sort colors
 	"color_rank_hike"			INTEGER,
-	"color_rank_global"		INTEGER UNIQUE,
-	"color_rank_global_h"	INTEGER UNIQUE, -- global_h = PREVIOUS_PICTURES + rank_value
+	"color_rank_global"		INTEGER, 				-- *UNIQUE*
+	"color_rank_global_h"	INTEGER, 				-- *UNIQUE* global_h = PREVIOUS_PICTURES + rank_value
 	"colors_count"				INTEGER,				-- # of dominant colors			>> for transfer animation only
 	"colors_rgb"					TEXT,						-- "R1,G1,B1|R2,G2,B2|..."	>> for transfer animation only
 	"colors_conf"					TEXT,						-- confidence value for each dominant color
@@ -43,7 +43,7 @@ DROP TABLE IF EXISTS "hikes";
 CREATE TABLE "hikes" (
 	"hike_id"							INTEGER UNIQUE,
 	"avg_altitude"				REAL,
-	"avg_altitude_rank"		INTEGER UNIQUE,
+	"avg_altitude_rank"		INTEGER,				-- *UNIQUE*
 	"start_time"					REAL UNIQUE,
 	"start_year"					INTEGER,
 	"start_month"					INTEGER,
@@ -59,7 +59,7 @@ CREATE TABLE "hikes" (
 	"color_hsv"						TEXT,
 	"color_rgb"						TEXT,
 	"color_rank_value"		TEXT,
-	"color_rank"					INTEGER UNIQUE,
+	"color_rank"					INTEGER,				-- *UNIQUE*
 	"pictures"						INTEGER,
 	"path" 								TEXT UNIQUE,
 	"created_date_time" 	TEXT DEFAULT CURRENT_TIMESTAMP,
