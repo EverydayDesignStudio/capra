accf6c6d8f300eabf35a765b565ca419e3b25838
Min <yoomy1203@hotmail.com>
2021-11-30 16:23:01 -0800

    Rewrite UI methods to avoid the use of mod() in sql queries

--- projector_slideshow.py
+++ projector_slideshow.py
@@ -74,7 +74,7 @@ class StatusScope(IntEnum):
 
 
 class StatusMode(IntEnum):
-    '''The order as described by the integer values is the order 
+    '''The order as described by the integer values is the order
     in which the modes will change'''
     __order__ = 'TIME COLOR ALTITUDE'
     TIME = 0
@@ -825,7 +825,7 @@ class MainWindow(QMainWindow):
             self.bottomUIContainer.layout.addWidget(self.scrollSpeedLabel)
 
         # Altitude Graph
-        altitudelist = self.sql_controller.ui_get_altitudes_for_hike_sortby('time', self.picture)
+        altitudelist = self.sql_controller.ui_get_altitudes_for_hike_sortby('time', self.picture, self.uiData.indexListForHike[self.picture.hike_id])
         print(altitudelist)
         self.altitudegraph = AltitudeGraph(False, altitudelist, percent_rank, self.picture.altitude)
         self.altitudegraph.setGraphicsEffect(UIEffectDropShadow())
@@ -833,7 +833,7 @@ class MainWindow(QMainWindow):
         # self.bottomUIContainer.layout.addItem(spacer)
 
         # Color Bar
-        colorlist = self.sql_controller.ui_get_colors_for_hike_sortby('time', self.picture)
+        colorlist = self.sql_controller.ui_get_colors_for_hike_sortby('time', self.picture, self.uiData.indexListForHike[self.picture.hike_id])
         self.colorbar = ColorBar(False, colorlist, percent_rank, self.picture.color_rgb)
         self.colorbar.setGraphicsEffect(UIEffectDropShadow())
         self.bottomUIContainer.layout.addWidget(self.colorbar)
@@ -1167,7 +1167,7 @@ class MainWindow(QMainWindow):
         if self.preload:
             print('use the preloaded UIData')
             if scope == StatusScope.HIKE:
-                hike = self.picture.hike_id 
+                hike = self.picture.hike_id
                 rank_timebar = self.sql_controller.ui_get_percentage_in_hike_with_mode('time', self.picture)
                 if mode == StatusMode.ALTITUDE:
                     altitudelist = self.uiData.altitudesSortByAltitudeForHike[hike]
